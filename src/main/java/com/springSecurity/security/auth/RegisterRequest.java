82a5fbe84596fa6398a6dcdf2afe27aaa7915f6d
DAnderson829 <dylandoku@gmail.com>
2025-09-19 08:51:47 -0400

    .

---
+++ src/main/java/com/springSecurity/security/auth/RegisterRequest.java
@@ -0,0 +1,12 @@
+package com.springSecurity.security.auth;
+
+public class RegisterRequest {
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String password;
+}
